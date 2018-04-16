d38ad600395da0a60b0cf7609f5984416ea5b5f8
mmcneeley <mmcneeley@gmail.com>
2018-04-16 18:13:04 -0400

    added basic crud functionality for users + posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
