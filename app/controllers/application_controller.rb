a5e6f557263e0acbc6eed93f7610c686b868ecca
mmcneeley <mmcneeley@gmail.com>
2018-04-17 15:42:06 -0400

    couple of small changes for the merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   require 'nokogiri'
   require 'open-uri'
   require 'metainspector'
   require 'net/http'
-  
+
   helper_method :web_scraper, :get_title, :get_image, :working_url?
   helper_method :logged_in?
   helper_method :current_user
+  helper_method :authorize
 
   def working_url?(url)
    url = URI.parse(url) rescue false
    url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
- end
+  end
 
   def web_scraper
     Nokogiri::HTML(open(url))
@@ -29,6 +30,7 @@ class ApplicationController < ActionController::Base
   def get_image
     page = MetaInspector.new(@post.url)
     page.images.best
+  end
 
   def current_user
     if session[:user_id]
