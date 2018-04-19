523eef9bf7c334235980f73ffc6b0ea240c93f9f
JMoloughney <github email address>
2018-04-19 12:48:24 -0400

    added a profanity validation and added the topic filter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,18 +3,24 @@ class Post < ApplicationRecord
   belongs_to :topic
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
-  has_many :post_tags, dependent: :destroy
-  has_many :tags, through: :post_tags, dependent: :destroy
+  has_many :post_tags
+  has_many :tags, through: :post_tags
   validates :title, presence: true
+  validate :foul_words
   # validates :url, url: { allow_blank: true }
 
-  def self.search(search)
-  
-  end
+def self.search(search)
 
-  # def self.search(search)
-  #   t = Tag.where("name like ?", "%#{search}%")
-  # end
+end
+
+def foul_words
+  bad_words = ["shit","fuck","ass","fucker","motherfucker","bitch", "slut", "fag", "pussy", "dick"]
+  bad_words.each do |word|
+    if title.include?(word) || content.include?(word)
+    errors.add(:foul_words,"- Wash your hands out with soap, that language ain't tolerated on Wroteit!")
+    end
+  end
+end
 
 
   def score
